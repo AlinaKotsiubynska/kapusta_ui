f552237212c9a0bf607a88911202ed4eb4110ca8
23birola <23birola@gmail.com>
2021-11-10 00:00:44 +0200

    created fetch for reports

--- src/components/Report/ReportPage/ReportPage.jsx
+++ src/components/Report/ReportPage/ReportPage.jsx
@@ -1,11 +1,42 @@
 // import { Link, useRouteMatch, useHistory, useLocation} from 'react-router-dom';
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 // import css from './ReportPage.module.css'
 import { ReportHeading } from '../ReportHeading';
 import { CategoriesList } from '../CategoriesList';
 import { Chart } from '../Chart';
+import { fetchAllCategories, fetchDataByDate } from 'services/reports-api';
+
+fetchAllCategories().then(response => console.log(response));
+const currentData = new Date();
+const currentYear = currentData.getFullYear();
+const currentMounth = currentData.getUTCMonth();
+console.log('Mounth', currentMounth);
+console.log('year', currentYear);
 
 export const ReportPage = () => {
+  const [allCategories, setAllCategories] = useState(null);
+  const [expensesByCategories, setExpensesByCategories] = useState(null);
+  const [expensesByDate, setExpensesByDate] = useState(null);
+  const [expensesBySubCategories, setExpensesSubByCategories] = useState(null);
+  const [mounth, setMounth] = useState(currentMounth);
+  const [year, setYear] = useState(currentYear);
+  console.log('data');
+
+  useEffect(() => fetchAllCategories().then(setAllCategories), []);
+  useEffect(() => fetchDataByDate().then(setExpensesByDate), [mounth, year]);
+  useEffect(() => {
+    const expensesByCategories = allCategories.map(category => {
+      const dataByCategory = expensesByDate.find(
+        item => item.categoryName === category,
+      );
+      const value = dataByCategory ? dataByCategory.value : '0';
+      category.value = value;
+      category.url = `../../svg/${category}`;
+      return category;
+    });
+    setExpensesByCategories(expensesByCategories);
+  }, [allCategories, expensesByDate]);
+
   return (
     <>
       <ReportHeading />
