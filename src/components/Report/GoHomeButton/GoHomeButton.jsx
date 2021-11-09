6668be44abeb6d46fe4c520546d8b16ee5ad5797
23birola <23birola@gmail.com>
2021-11-09 19:55:36 +0200

    added code

---
+++ src/components/Report/GoHomeButton/GoHomeButton.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import { HOME } from 'helpers/constants/routes.constants';
+import css from './BackButton.module.css';
+
+export const GoHomeLink = () => {
+  return (
+    <Link to={`/${HOME}`} className={css.backBtn}>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M21 11H6.83L10.41 7.41L9 6L3 12L9 18L10.41 16.59L6.83 13H21V11Z"
+          fill="#FF751D"
+        />
+      </svg>
+      Вернуться на главную
+    </Link>
+  );
+};
