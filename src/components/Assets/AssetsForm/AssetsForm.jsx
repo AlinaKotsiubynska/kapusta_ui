bc19eb76804e7a82e6caf15dce4644c932fab438
Serhii Tomashkevich <19tomat@gmail.com>
2021-11-11 21:08:37 +0200

    add styles

--- src/components/Assets/AssetsForm/AssetsForm.jsx
+++ src/components/Assets/AssetsForm/AssetsForm.jsx
@@ -3,6 +3,7 @@ import styles from './AssetsForm.module.scss';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { ReactComponent as CalendIcon } from 'assets/icons/calendar.svg';
+import { ReactComponent as Calculator } from 'assets/icons/calculator.svg';
 import { useState } from 'react';
 import { format } from 'date-fns';
 
@@ -59,7 +60,15 @@ export const AssetsForm = () => {
             </option>
           ))}
         </select>
-        <div className={styles.calc}>calc</div>
+        <div className={styles.calculatorWrapper}>
+          <Calculator className={styles.calculatorIcon} />
+          <input
+            className={styles.calc}
+            type="number"
+            step="0.01"
+            placeholder="0,00"
+          />
+        </div>
         <Button type="submit">ввод</Button>
         <Button type="button">очистить</Button>
       </form>
