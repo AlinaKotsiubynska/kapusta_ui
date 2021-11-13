0da4f577c84ba310956292d9e9463e83b93fd5dd
alla-andronova <allaitcreative@gmail.com>
2021-11-13 19:05:59 +0200

    add auth logic

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,5 +1,7 @@
 import { Route, Switch, Redirect, useRouteMatch } from 'react-router-dom';
 import { LOGIN, SIGNUP } from 'helpers/constants/routes.constants';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import SignupForm from '../../components/SignupForm/SignupForm';
 
 export const AuthPage = () => {
   const match = useRouteMatch();
@@ -10,9 +12,11 @@ export const AuthPage = () => {
       <Switch>
         <Route path={`${match.path}/${LOGIN}`}>
           <p>Login Form</p>
+          <LoginForm />
         </Route>
         <Route path={`${match.path}/${SIGNUP}`}>
           <p>Sign Up Form</p>
+          <SignupForm />
         </Route>
         <Redirect to={`${match.path}/${LOGIN}`} />
       </Switch>
