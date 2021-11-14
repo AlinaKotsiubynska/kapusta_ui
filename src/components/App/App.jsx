7b817c0c03fe85d56655ca7093ac0fa37d62acf1
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-11-15 00:40:05 +0200

    get user from local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,42 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { AuthPage, HomePage } from 'pages';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Context } from 'components/Context';
 import { AUTH, HOME } from 'helpers/constants/routes.constants';
 import { USER_CONTEXT_DEFAULT } from 'helpers/constants/contexst.constants';
 import s from './App.module.scss';
+import axios from 'axios';
 // import Header from './Header';
 // import Modal from './../shared/Modal/Modal';
 
 export default function App() {
   const [userContext, setUserContext] = useState(USER_CONTEXT_DEFAULT);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (token) {
+      axios.defaults.baseURL = 'http://app-kapusta.herokuapp.com/api';
+      axios.defaults.headers.common.Authorization = 'Bearer ' + token;
+      axios
+        .get('/users/current')
+        .then(({ data }) => {
+          setUserContext(state => {
+            return {
+              ...state,
+              token,
+              user: {
+                ...state.user,
+                ...data.user,
+              },
+              authenticated: true,
+            };
+          });
+        })
+        .catch(err => console.log(err));
+    }
+  }, []);
+
+  useEffect(() => {}, [userContext]);
 
   return (
     <Context.Provider value={{ userContext, setUserContext }}>
