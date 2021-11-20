7dadbd109020af94d1520924c83ce1cb40129470
Serhii Tomashkevich <19tomat@gmail.com>
2021-11-20 14:10:19 +0200

    run husky

--- src/components/Assets/Api/Api.js
+++ src/components/Assets/Api/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://app-kapusta.herokuapp.com/api';
-axios.defaults.headers.common['Authorization'] =
-  'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkxOGY4ZTFkMTVmOGU1OWVjNzQ1ODUiLCJpYXQiOjE2MzcxNzQ5MTJ9.W4wkkNPWjgWJ6mNfdedRaQ0bojUxdZUdwqgL1GCp2tk';
+// axios.defaults.baseURL = 'https://app-kapusta.herokuapp.com/api';
+// axios.defaults.headers.common['Authorization'] =
+//   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTkxOGY4ZTFkMTVmOGU1OWVjNzQ1ODUiLCJpYXQiOjE2MzcxNzQ5MTJ9.W4wkkNPWjgWJ6mNfdedRaQ0bojUxdZUdwqgL1GCp2tk';
 
 export const getCategories = async () => {
   return await axios.get('categories');
