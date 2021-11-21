a6e69123e1d37e73ee1d7386c8c2704b965923fc
Julia Kutsyn <yuliyaflm@gmail.com>
2021-11-21 23:18:28 +0200

    adds hero styles and some images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,10 @@
+import s from '../Hero/Hero.module.scss';
+
 export const Hero = () => {
   return (
-    <div>
-      <h1 className="title">Kapu$ta</h1>
-      <p className="desc">Smart Finance</p>
+    <div className={s.wrapper}>
+      <h1 className={s.title}>Kapu$ta</h1>
+      <p className={s.desc}>Smart Finance</p>
     </div>
   );
 };
