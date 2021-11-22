b33c48da74f1486e2daccd5c5cb262d423abce94
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-11-22 13:49:25 +0200

    +add: close modal on backdrop click

--- src/components/shared/Modal/Modal.jsx
+++ src/components/shared/Modal/Modal.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import { useRef, useEffect } from 'react';
 import s from '../Modal/Modal.module.scss';
 
 function Modal({ active, setActive, children, onSubmitClick, onCanselCLick }) {
+  const modalRef = useRef(null);
+  const backdropRef = useRef(null);
+
+  const closeModal = e => {
+    if (e.target === backdropRef.current) {
+      onCanselCLick();
+    }
+  };
+  useEffect(() => {
+    modalRef.current = document.querySelector('#modal-logout');
+    backdropRef.current = document.querySelector('#modal-backdrop');
+  }, []);
   return (
-    <div
-      className={active ? 'modal active' : 'modal'}
-      onClick={() => setActive(false)}
-    >
+    <div id={'modal-backdrop'} className={s.overlay} onClick={closeModal}>
       <div
-        className={active ? 'modal-content active' : 'modal-content'}
-        onClick={e => e.stopPropagation()}
+        id={'modal-logout'}
+        className={active ? [s.modal, s.active].join(' ') : s.modal}
       >
-        {children}
-        <button className={s.modalBtnYes} onClick={onSubmitClick}>
-          да
-        </button>
-        <button className={s.modalBtnNo} onClick={onCanselCLick}>
-          нет
-        </button>
+        <div
+          className={
+            active
+              ? [s['modal-content'], s.active].join(' ')
+              : s['modal-content']
+          }
+        >
+          {children}
+          <div className={s.modalBtns}>
+            <button className={s.modalBtnYes} onClick={onSubmitClick}>
+              да
+            </button>
+            <button className={s.modalBtnNo} onClick={onCanselCLick}>
+              нет
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
